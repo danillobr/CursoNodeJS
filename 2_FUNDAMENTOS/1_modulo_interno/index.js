4b7b65b7ea929fedc38c8c6185439c27ced90f3b
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-10 11:16:35 -0300

    criando o diretório 2_FUNDAMENTOS

---
+++ 2_FUNDAMENTOS/1_modulo_interno/index.js
@@ -0,0 +1,6 @@
+const meuModulo = require('./meu_modulo')
+const soma = meuModulo.soma
+
+soma (2, -9)
+soma (1, 8)
+soma (14, 3)
---
+++ 2_FUNDAMENTOS/2_export_import/index.mjs
@@ -0,0 +1,4 @@
+import soma from './meu_modulo.mjs'
+
+soma(2, 4)
+soma(-2, 7)
