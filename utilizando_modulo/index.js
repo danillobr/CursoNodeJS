77e1e41287a6ebfb43b9361dab9f6666c0d19ddc
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-09 22:54:22 -0300

    utilizando o modulo fire system

--- utilizando_modulo/index.js
+++ utilizando_modulo/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs') // importando o modulo file system
 
 fs.readFile('arquivo_de_texto.txt', 'utf8', (err, data) => {
-
-    if(err){
+    if (err) {
         console.log(err)
+        return
     }
 
     console.log(data)
