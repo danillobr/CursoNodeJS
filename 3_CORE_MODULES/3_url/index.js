81f47f20e4e480d5e2e66f845c0ae13305bbf846
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-12 02:13:37 -0300

    Criando o diretório 4_http_com_url

--- 3_CORE_MODULES/3_url/index.js
+++ 3_CORE_MODULES/3_url/index.js
@@ -4,5 +4,6 @@ const parseUrl = new url.URL(address)
 
 console.log(parseUrl.host)
 console.log(parseUrl.pathname)
+console.log(parseUrl.search)
 console.log(parseUrl.searchParams)
 console.log(parseUrl.searchParams.get('produtos'))
