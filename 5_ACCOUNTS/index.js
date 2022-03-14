bf7ea2b37df2e6b9e235a77b73d0411eea8c01b6
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-14 01:31:31 -0300

    criando a function operaction

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -4,5 +4,30 @@ const inquerer = require('inquirer')
 
 // modulos externos
 const fs = require('fs')
+const Choices = require('inquirer/lib/objects/choices')
 
-console.log('Iniciamos o Accounts')
+operation()
+
+// operacions
+function operation() {
+    inquerer.prompt([{
+        type: 'list',
+        name: "action",
+        message: "O que deseja fazer?",
+        choices: [
+            'Criar Conta',
+            'Consultar Saldo',
+            'Depositar',
+            'Sacar',
+            'Sair'
+        ]
+    }]).then((answer) => {
+        const action = answer['action']
+
+        if (action === 'Criar Conta') {
+            createAccount()
+        }
+    }).catch((err) => {
+        console.log(err)
+    })
+}
