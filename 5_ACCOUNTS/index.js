19f25ca925e314a42c95cc04e6527449c2d14407
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-14 01:35:31 -0300

    criando a function createAccount

--- 5_ACCOUNTS/index.js
+++ 5_ACCOUNTS/index.js
@@ -31,3 +31,8 @@ function operation() {
         console.log(err)
     })
 }
+
+function createAccount() {
+    console.log(chalk.bgGreen.black('Parabéns por escolher nosso banco!'))
+    console.log(chalk.green('Defina as opções da sua conta a seguir'))
+}
