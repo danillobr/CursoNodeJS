859cf9f4d1d0723baf74c284fc6b08526ec639ec
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-03-15 23:13:31 -0300

    criando o diretório 5_else

---
+++ 7_TEMPLATE_ENGINE/5_else/index.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+app.get('/dashbord', (req, res) => {
+    res.render('dashbord')
+})
+
+app.get('/', (req, res) => {
+
+    const user = {
+        name: "Danillo",
+        surname: "Rodrigues",
+        age: "24"
+    }
+    const palavra = 'teste'
+
+    const auth = true
+
+    const approved = false
+
+    res.render('home', { user: user, palavra, auth, approved })
+})
+
+app.listen(3000, () => {
+    console.log('App funcionando')
+})
